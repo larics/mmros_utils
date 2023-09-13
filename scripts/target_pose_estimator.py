a9c0fcef2107370a3cbaf2b8dec15ec6caa61db0
Filip Zoric <fzoric8@gmail.com>
2023-09-13 17:44:54 +0200

    Added some fixes to the target pose estimator so we have litter positions

--- scripts/target_pose_estimator.py
+++ scripts/target_pose_estimator.py
@@ -18,13 +18,15 @@ class TargetPoseEstimator:
         self.dpth_reciv = False
         
         # TODO: Create transformations 
-        self.T_world_uav = None # TODO: that's from the 
-        self.T_cam_camr = self.getRotXT(pitch_angle)
-        self.T_uav_cam = np.array([[0, -1, 0, 0], 
-                                   [0, 0, -1, 0], 
-                                   [1, 0, 0, 0], 
-                                   [0, 0, 0, 1]])
-        self.T_complete = np.matmul(self.T_uav_cam, self.cam_camr)
+        self.T_world_uav = None # TODO: that's from the
+        pitch_ang = 60 
+        # Remove cam rotation (pitch 60)
+        self.T_cam_camr = self.getRotXT(np.radians(-pitch_angle))
+        # Move cam to the base of the UAV
+        self.T_uav_cam = np.array([[0, 0, 1, 0], 
+                                   [-1, 0, 0, 0], 
+                                   [0, -1, 0, 0], 
+                                   [0, 0, 0, 1]])        
 
     def detected_objects_cb(self, data):
         # Clear the previously detected centroids
@@ -43,7 +45,6 @@ class TargetPoseEstimator:
 
         # Get current depths from depth cam --> TODO: Change read_points with cam_homography
         depths = pc2.read_points(pcl, [axis], False, uvs=indices)
-
         return depths
 
     def check_nan(self, val): 
@@ -56,12 +57,11 @@ class TargetPoseEstimator:
     def getRotXT(self, angle): 
         
         return np.array([[1, 0, 0, 0], 
-                         [0, np.cos(angle), np.sin(angle), 0], 
+                         [0, np.cos(angle), -np.sin(angle), 0], 
                          [0, np.sin(angle), np.cos(angle), 0], 
                          [0, 0, 0, 1]]
                          
-    def createCPArray(self, transform=None`tea):
-        
+    def createCentroidPositionArray(self, transform=None):
         cp_array = CentroidPoseEstimateArray()
         for i, axis in enumerate('xyz'): 
             gen_dpths = self.get_depths(self.pcl, self.detected_centroids, axis='{}'.format(axis))
@@ -76,48 +76,28 @@ class TargetPoseEstimator:
             if axis == 'z': 
                 for kz,z in enumerate(gen_dpths): 
                     cp_array.poses[kz].pose.z = self.check_nan(z[0])
-                    
             if transform: 
-                
                 for i, pose in enumerate(cp_array.poses):
-                    
                     p = np.array([pose.x, pose.y, pose.z, 1])
-                    p_new = np.matmul(T, p) 
+                    p = np.matmul(self.T_cam_camr, p)
+                    p_new = np.matmul(self.T_uav_cam, p) 
+                    cp_array.poses[i].x = p_new[0]
+                    cp_array.poses[i].y = p_new[1]
+                    cp_array.poses[i].z = p_new[2]
+        return cp_array
                     
-        
-        
 
     def run(self):
         rate = rospy.Rate(10)  # Update rate, adjust as needed
-
         while not rospy.is_shutdown():
             # Process the detected centroids if needed
             start_condition = self.detected_centroids and self.dpth_reciv
             if start_condition:
                 rospy.loginfo("Detected Centroids: {}".format(self.detected_centroids))
-                cp_array = CentroidPoseEstimateArray()
-                for i, axis in enumerate("xyz"):
-                    gen_dpths =  self.get_depths(self.pcl, self.detected_centroids, axis="{}".format(axis))
-                    # Assign values for each of the detected points
-                    if axis == "x": 
-                        for kx, x in enumerate(gen_dpths): 
-                            cp_array.poses.append(CentroidPoseEstimate())
-                            cp_array.poses[kx].pose.x = self.check_nan(x[0])
-                    if axis == "y": 
-                        for ky, y in enumerate(gen_dpths):
-                            cp_array.poses[ky].pose.y = self.check_nan(y[0])
-                    if axis == "z": 
-                        for kz, z in enumerate(gen_dpths): 
-                            cp_array.poses[kz].pose.z = self.check_nan(z[0])
-
-                    self.pose_pub.publish(cp_array)
-
-                    #_dpth = [i for i in gen_dpths]
-                    #print("Calculated first point depth is: {}".format(_dpth))
-                    
+                cp_array = createCentroidPositionArray(transform = True)
+                self.pose_pub.publish(cp_array) 
             else: 
                 rospy.logwarn("Depth reciv: {}, Detection reciv: {}".format(self.dpth_reciv, self.detected_centroids_reciv))
-            
             rate.sleep()
 
 if __name__ == '__main__':
